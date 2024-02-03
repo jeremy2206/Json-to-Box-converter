5dfb50bdc2b6dd295c40f7e3741d40b0ec38e9b9
Jerem <55030784+jeremy2206@users.noreply.github.com>
2024-02-03 21:39:31 +0100

    Add files via upload

--- json to box.py
+++ json to box.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from PIL import ImageTk, Image, ImageFont, ImageDraw
 import json
 import os
 from datetime import datetime
@@ -16,13 +17,13 @@ if not os.path.exists(box_folder):
 
 def charger_fichier():
     global data, json_file_name  # Déclarer data et json_file_name comme globales
-    file_path = filedialog.askopenfilename(title="Sélectionner un fichier JSON", filetypes=[("Fichiers JSON", "*.json")])
+    file_path = filedialog.askopenfilename(title="Select a JSON file", filetypes=[("JSON File", "*.json")])
     if file_path:
         json_file_name = os.path.splitext(os.path.basename(file_path))[0]
         with open(file_path, 'r') as file:
             data = json.load(file)
         convert_button["state"] = tk.NORMAL  # Activer le bouton "Convert" une fois le fichier chargé
-        info_label.config(text=f"Fichier {json_file_name}.json chargé avec succès.")
+        info_label.config(text=f"File : {json_file_name}.json loads with succeed.")
         return data
 
 def convertir():
@@ -35,8 +36,8 @@ def convertir():
             
             with open(output_file_path, 'w') as result_file:
                 result_file.write(converted_data)
-            info_label.config(text=f"Conversion terminée. Vérifiez le fichier {output_file_name}.")
-            print(f"Conversion terminée. Vérifiez le fichier {output_file_name}.")
+            info_label.config(text=f"Conversion complete. Check the file : {output_file_name}.")
+            print(f"Conversion complete. Check the file : {output_file_name}.")
             
             # Ouvrir automatiquement le fichier après la conversion
             os.startfile(output_file_path)
@@ -44,8 +45,8 @@ def convertir():
             # Désactiver le bouton "Convert" après la conversion
             convert_button["state"] = tk.DISABLED
     except Exception as e:
-        print(f"Erreur lors de la conversion : {e}")
-        info_label.config(text=f"Erreur lors de la conversion. Vérifiez la console pour plus d'informations.")
+        print(f"Error during conversion : {e}")
+        info_label.config(text=f"Error during conversion")
 
 def convertir_json(data):
     result = ""
@@ -95,22 +96,73 @@ def convertir_json(data):
 def generate_output_file_name(json_file_name):
     # Générer le nom du fichier avec le format NOMDUJSON_YYMMDDHHMMSS
     current_time = datetime.now()
-    return f"{json_file_name}_{current_time.strftime('%y%m%d%H%M%S')}.txt"
+    return f"{json_file_name} {current_time.strftime('%y-%m-%d-%H-%M-%S')}.txt"
+
+#-------------------------------------------------------------------------------------------------
 
 root = tk.Tk()
-root.title("Convertisseur JSON")
+root.title("JSON To Box Converter")
+root.geometry("500x200")
+root.resizable(False, False)
+#IconPath = os.path.abspath("assets/icon.ico")
+#root.iconbitmap(IconPath)
+
+#-------------------------------------------------------------------------------------------------
+
+# Global Background
+Background = tk.Label(root, bg="#383838")
+Background.place(x=0, y=0, width=1000, height=1000)
+
+#-------------------------------------------------------------------------------------------------
+#   **--Title and Background--** 
+
+FontMojangles = os.path.abspath("assets/Mojangles.ttf")
+TitleAppText = "JSON To Box Converter"
+TitleAppFontSize = 25
+TitleAppFont = ImageFont.truetype(FontMojangles, TitleAppFontSize)
+
+
+# Image and Border
+TitleAppImageWidth = 500
+TitleAppImageHeight = 35
+TitleAppImage = Image.new("RGBA", (TitleAppImageWidth, TitleAppImageHeight), (255, 255, 255, 0))
+TitleAppDraw = ImageDraw.Draw(TitleAppImage)
+TitleAppOutlineColor = (0, 0, 0)
+TitleAppOutlinePosition = (89.5, 4)
+TitleAppDraw.text(TitleAppOutlinePosition, TitleAppText, font=TitleAppFont, fill=TitleAppOutlineColor)
+
+# Draw Text
+TitleAppTextColor = (255, 255, 255)
+TitleAppTextPosition = (87, 2)
+TitleAppDraw.text(TitleAppTextPosition, TitleAppText, font=TitleAppFont, fill=TitleAppTextColor)
+
+# Convert Image to tk
+TitleAppImagetk = ImageTk.PhotoImage(TitleAppImage)
+
+# Show Image
+TitleApp = tk.Label(root, image=TitleAppImagetk, bg="#585858")
+TitleApp.place(x=0, y=0)
+
+#-------------------------------------------------------------------------------------------------
+
+root.columnconfigure(0, minsize=50)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(0, weight=1)
+root.grid_columnconfigure((0,1,2), minsize=0, weight=1)
 
 # Bouton pour charger le fichier JSON
-load_button = tk.Button(root, text="Charger un fichier JSON", command=charger_fichier)
-load_button.pack(pady=10)
+load_button = tk.Button(root, text="Load a JSON file", command=charger_fichier)
+load_button.grid(row=1, column=1,pady=10)
+load_button.configure(relief="solid", bd=2)
 
 # Bouton pour convertir le JSON et créer un fichier texte
 convert_button = tk.Button(root, text="Convert", command=convertir, state=tk.DISABLED)
-convert_button.pack(pady=10)
+convert_button.grid(row=2, column=1,pady=10)
+convert_button.configure(relief="solid", bd=2)
 
 # Étiquette pour afficher les messages d'information
 info_label = tk.Label(root, text="")
-info_label.pack(pady=10)
+info_label.grid(row=3, column=1,pady=20)
 
 
 root.mainloop()
